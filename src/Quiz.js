fcfe9ccabc50837af2b17d849c294bc5ccd08c70
Hoang <chun.hoang@accenture.com>
2020-05-01 16:01:02 -0400

    module7

--- src/Quiz.js
+++ src/Quiz.js
@@ -16,11 +16,15 @@ class Quiz extends Component {
     this.setState({quiz_position: this.state.quiz_position+1});
   }
 
+  handleResetClick() {
+    this.setState({quiz_position: 1});
+  }
+
   render() {
     const isQuizEnd = this.state.quiz_position-1 === quizData.quiz_questions.length;
     let contents = null;
     if (isQuizEnd) {
-      contents = <QuizEnd />
+      contents = <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} />
     } else {
       contents = <QuizQuestion
                   quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}
